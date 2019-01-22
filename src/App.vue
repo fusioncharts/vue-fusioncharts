b21374cc54f56caeddd6b1f05e72d922a5e9add2
Rohan Dey <rohanoid5@gmail.com>
2019-01-22 17:06:48 +0530

    Added timeseries integration section

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    <the-header :wrapper-github-repo-url='config.githubRepo' />
+    <the-header :wrapper-github-repo-url="config.githubRepo"/>
     <div class="page-container">
       <div id="mainContainer" class="main-container">
-            <WrapperInfo :config="config" />
-            <SamplesViewer
-              :samplesConfig="sampleConfig"
-            />
-          <div class="container container-1200 info-wrapper pt-4" style="z-index:0">
-            <QuickStartViewer />
-            <app-footer :issuesURL="config.issuesURL" />
-          </div>
+        <WrapperInfo :config="config"/>
+        <SamplesViewer :samplesConfig="sampleConfig"/>
+        <div class="container container-1200 info-wrapper pt-4" style="z-index:0">
+          <QuickStartViewer/>
+          <FusionTimeUsage/>
+          <app-footer :issuesURL="config.issuesURL"/>
+        </div>
         <div id="footbar" class="row text-center mt-2">
-            <div class="col-12">
-                <p class="foot-item pt-4">© 2002-2018 InfoSoft Global Private Limited. All Rights Reserved.</p>
-            </div>
+          <div class="col-12">
+            <p
+              class="foot-item pt-4"
+            >© 2002-2018 InfoSoft Global Private Limited. All Rights Reserved.</p>
+          </div>
         </div>
       </div>
     </div>
@@ -22,30 +23,32 @@
 </template>
 
 <script>
-import TheHeader from './components/Header'
-import WrapperInfo from './components/WrapperInfo'
-import AppFooter from './components/Footer'
-import QuickStartViewer from './components/QuickStartViewer'
-import SamplesViewer from './components/SamplesViewer'
+import TheHeader from "./components/Header";
+import WrapperInfo from "./components/WrapperInfo";
+import AppFooter from "./components/Footer";
+import QuickStartViewer from "./components/QuickStartViewer";
+import SamplesViewer from "./components/SamplesViewer";
+import FusionTimeUsage from "./components/FusionTimeUsage";
 
-import config from './config/wrapper.config'
-import sampleConfig from './config/sample.config'
+import config from "./config/wrapper.config";
+import sampleConfig from "./config/sample.config";
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
       config: config,
       sampleConfig
-    }
+    };
   },
   components: {
     TheHeader,
     WrapperInfo,
     SamplesViewer,
     QuickStartViewer,
+    FusionTimeUsage,
     AppFooter
   }
-}
+};
 </script>
 
 
